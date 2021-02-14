1bc9d993008763168d7f90f80beb5c35b712f912
Erdem <245907@student.pwr.edu.pl>
2021-02-14 20:19:25 +0100

    0.2 changes

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ import requests
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-
+#READER CODE IMPLEMENT IT LATER
 #url = input('Enter - ')
 #html = urllib.request.urlopen(url, context=ctx).read()
 #soup = BeautifulSoup(html, 'html.parser')
 #count = 0
 # Retrieve all of the anchor tags
 #tags = soup('a')
-import requests
-
+#miners local ip
 login_url = 'http://192.168.0.108/cgi-bin/minerStatus.cgi'
 data = {
     'username': 'root',
@@ -28,3 +27,20 @@ with requests.Session() as s:
     index_page= s.get('http://192.168.0.108/cgi-bin/minerStatus.cgi')
     soup = BeautifulSoup(index_page.text, 'html.parser')
     print(soup.title)
+
+
+#future code:refactor it!
+'''
+page = requests.get("http://192.168.0.108/cgi-bin/minerStatus.cgi")
+soup = BeautifulSoup(page.content, 'html.parser')
+warning = soup.find(id="degreet")
+mineritems = seven_day.find_all(class_="tombstone-container")
+degree = forecast_items[0]
+print(degree.prettify())
+    while true"
+    if degree > 80:
+        print("Warning",warning)
+        usrinp =inp("Press p to quit")
+        if usrinp  == q:
+            quit()
+'''
