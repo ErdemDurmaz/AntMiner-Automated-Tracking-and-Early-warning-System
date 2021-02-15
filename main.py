8d2e047987c6b4b1c89e55d96976c56c99cb54a9
Erdem <245907@student.pwr.edu.pl>
2021-02-15 16:52:12 +0100

    0.3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 import requests
-
+from base64 import b64encode
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -15,19 +15,6 @@ ctx.verify_mode = ssl.CERT_NONE
 # Retrieve all of the anchor tags
 #tags = soup('a')
 #miners local ip
-login_url = 'http://192.168.0.108/cgi-bin/minerStatus.cgi'
-data = {
-    'username': 'root',
-    'password': 'root'
-}
-
-with requests.Session() as s:
-    response = requests.post(login_url , data)
-    print(response.text)
-    index_page= s.get('http://192.168.0.108/cgi-bin/minerStatus.cgi')
-    soup = BeautifulSoup(index_page.text, 'html.parser')
-    print(soup.title)
-
 
 #future code:refactor it!
 '''
